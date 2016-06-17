a5da94c0f6dc8474053a092ec4580594f5c1c324
Komal Bandi <info@pidataconsulting.com.au>
2016-06-17 15:21:14 +1000

    updated

--- source/javascripts/forsale.js
+++ source/javascripts/forsale.js
@@ -5,12 +5,17 @@ $(document).ready(function(){
     $(document).on('change', '#sortcombo', function() {
         if($('#sortcombo option:selected').val()=='suburb-ASC')
         {
-            window.location.replace('/index.html');
+            window.location.replace('/for-sale/index-ASC.html');
         }
 
         if($('#sortcombo option:selected').val()=='suburb-DES')
         {
-            window.location.replace('/index-reverse.html');
+            window.location.replace('/for-sale/index-reverse.html');
+        }
+
+        if($('#sortcombo option:selected').val()=='updated-DES')
+        {
+            window.location.replace('/for-sale/index.html');
         }
     });
 });
