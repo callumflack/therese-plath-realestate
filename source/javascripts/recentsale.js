bb9e4af54e4f7e103a5567edb542875cbbd70524
Komal Bandi <info@pidataconsulting.com.au>
2016-06-17 15:37:56 +1000

    new changes to recent sales.

---
+++ source/javascripts/recentsale.js
@@ -0,0 +1,21 @@
+/**
+ * Created by bandi on 17/06/2016.
+ */
+$(document).ready(function(){
+    $(document).on('change', '#sortcombo', function() {
+        if($('#sortcombo option:selected').val()=='suburb-ASC')
+        {
+            window.location.replace('/recent-sales/index-ASC.html');
+        }
+
+        if($('#sortcombo option:selected').val()=='suburb-DES')
+        {
+            window.location.replace('/recent-sales/index-reverse.html');
+        }
+
+        if($('#sortcombo option:selected').val()=='updated-DES')
+        {
+            window.location.replace('/recent-sales/index.html');
+        }
+    });
+});
