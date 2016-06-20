8768d236043a739597969b4665575df3ceaa7dec
Callum Flack <callumflack@gmail.com>
2016-06-20 14:56:07 +1000

    fixes responsiveness for all property list templates
    I’ve also updated the ‘Z-A’ template name & JS to be `index-DES.html`
    so it has the same naming pattern everywhere.

--- source/javascripts/forsale.js
+++ source/javascripts/forsale.js
@@ -2,20 +2,20 @@
  * Created by bandi on 17/06/2016.
  */
 $(document).ready(function(){
-    $(document).on('change', '#sortcombo', function() {
-        if($('#sortcombo option:selected').val()=='suburb-ASC')
-        {
-            window.location.replace('/for-sale/index-ASC.html');
-        }
+	$(document).on('change', '#sortcombo', function() {
+		if($('#sortcombo option:selected').val()=='suburb-ASC')
+		{
+			window.location.replace('/for-sale/index-ASC.html');
+		}
 
-        if($('#sortcombo option:selected').val()=='suburb-DES')
-        {
-            window.location.replace('/for-sale/index-reverse.html');
-        }
+		if($('#sortcombo option:selected').val()=='suburb-DES')
+		{
+			window.location.replace('/for-sale/index-DES.html');
+		}
 
-        if($('#sortcombo option:selected').val()=='updated-DES')
-        {
-            window.location.replace('/for-sale/index.html');
-        }
-    });
+		if($('#sortcombo option:selected').val()=='updated-DES')
+		{
+			window.location.replace('/for-sale/index.html');
+		}
+	});
 });
